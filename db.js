b48564973cef528c9c3ad42738d10ad89a1bef39
guido jacofsky <166538636+guidojacoo@users.noreply.github.com>
2025-09-10 14:01:13 -0300

    Add env example and ignore .env

--- db.js
+++ db.js
@@ -1,14 +1,21 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+import fs from "fs";
+
+dotenv.config();
+
 const { Pool } = pkg;
 
 export const pool = new Pool({
-    host: "ep-spring-night-acu7toll-pooler.sa-east-1.aws.neon.tech",
-    user: "neondb_owner",
-    password: "npg_gBKFm9ETa8CQ",
-    database: "neondb",
-    port: 5432,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
     ssl: {
-        rejectUnauthorized: false,
+        ca: fs.readFileSync(process.env.DB_SSL_CA),
+        key: fs.readFileSync(process.env.DB_SSL_KEY),
+        cert: fs.readFileSync(process.env.DB_SSL_CERT),
     },
 });
 
